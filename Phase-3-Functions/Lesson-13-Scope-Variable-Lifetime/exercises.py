891fb88c3e6728044bc4df15699062a2fcf0ba13
jnvrr.tinker.lab <jnvrr.stemlab@gmail.com>
2025-12-27 16:57:51 +0530

    Add Lesson 13: Scope and Variable Lifetime - exercises.py with 10 practice problems

---
+++ Phase-3-Functions/Lesson-13-Scope-Variable-Lifetime/exercises.py
@@ -0,0 +1,45 @@
+"""
+Lesson 13: Scope and Variable Lifetime - Exercises
+"""
+
+# EXERCISE 1: Local vs Global Variables
+# TODO: Create two functions that use the same variable name but in different scopes
+
+
+# EXERCISE 2: Modify Global Variable
+# TODO: Create a function that safely modifies a global variable using 'global' keyword
+
+
+# EXERCISE 3: Nested Function Scope
+# TODO: Create nested functions and use 'nonlocal' to modify enclosing scope variable
+
+
+# EXERCISE 4: Variable Shadowing
+# TODO: Demonstrate variable shadowing (local variable hiding global variable)
+
+
+# EXERCISE 5: Check Variable Existence
+# TODO: Create a function that checks if a variable exists in different scopes
+
+
+# EXERCISE 6: Local Variable Lifetime
+# TODO: Create functions that show when local variables are created and destroyed
+
+
+# EXERCISE 7: LEGB Rule Demonstration
+# TODO: Create nested functions demonstrating Local, Enclosing, Global, Built-in lookup
+
+
+# EXERCISE 8: Avoid Global Variable Issues
+# TODO: Refactor code that relies on globals to use parameters instead
+
+
+# EXERCISE 9: Dictionary as Namespace
+# TODO: Use vars() and locals() to explore function namespace
+
+
+# EXERCISE 10: Scope with Loops and Conditionals
+# TODO: Demonstrate that loop/conditional variables are accessible after block
+
+
+print("=== Lesson 13 Exercises ===")
