6bb08008e3d295353fe7b6e65ecd56b7733497f4
jnvrr.tinker.lab <jnvrr.stemlab@gmail.com>
2025-12-27 16:49:06 +0530

    Add Lesson 12: Function Parameters and Return Values - exercises.py with 10 practice problems

---
+++ Phase-3-Functions/Lesson-12-Function-Parameters-Return-Values/exercises.py
@@ -0,0 +1,45 @@
+"""
+Lesson 12: Function Parameters and Return Values - Exercises
+"""
+
+# EXERCISE 1: Create a function that takes 2 numbers and returns their sum
+# TODO: Write greet_sum function
+
+
+# EXERCISE 2: Create a function that takes a number and returns its absolute value
+# TODO: Write abs_value function
+
+
+# EXERCISE 3: Create a function that takes 3 numbers and returns their average
+# TODO: Write average function
+
+
+# EXERCISE 4: Create a function that takes a string and returns it reversed
+# TODO: Write reverse_string function
+
+
+# EXERCISE 5: Create a function that returns multiple values (name, age, city)
+# TODO: Write get_person_info function
+
+
+# EXERCISE 6: Create a function that validates if a password is strong (8+ chars, has number)
+# TODO: Write is_strong_password function
+
+
+# EXERCISE 7: Create two functions where one calls the other (nested calls)
+# TODO: Write calculate_discount and apply_discount functions
+
+
+# EXERCISE 8: Create a function that takes variable parameters (list of numbers) and returns sum
+# TODO: Write sum_all function
+
+
+# EXERCISE 9: Create a function that returns different values based on conditions
+# TODO: Write get_grade function (score -> letter grade)
+
+
+# EXERCISE 10: Create a function with optional parameters (default values)
+# TODO: Write calculate_shipping function (price, weight, express=False)
+
+
+print("=== Lesson 12 Exercises ===")
